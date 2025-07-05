2ceb260d26f54573f0b5875101e072ee5f2742cb
Ourwillxyz <admin@ourwill.xyz>
2025-07-05 16:33:12 +0300

    Create logout.js
    Created logout API to clear token cookie

---
+++ pages/api/logout.js
@@ -0,0 +1,13 @@
+// pages/api/logout.js
+import { serialize } from 'cookie';
+
+export default function handler(req, res) {
+  const cookie = serialize('token', '', {
+    httpOnly: true,
+    path: '/',
+    expires: new Date(0),
+  });
+
+  res.setHeader('Set-Cookie', cookie);
+  res.status(200).json({ message: 'Logged out' });
+}
