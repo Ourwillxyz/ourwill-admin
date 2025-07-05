114026450b37501abc88c20306dea3945e49e9ce
Ourwillxyz <admin@ourwill.xyz>
2025-07-05 16:07:38 +0300

    Create dashboard.js
    Mock dashboard handler

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,9 @@
+// pages/admin/dashboard.js
+
+export default function AdminDashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>Welcome to the Admin Dashboard</h1>
+    </div>
+  );
+}
