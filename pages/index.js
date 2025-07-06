72aee36722d35328c268655e23859dd8f3f3eb42
Ourwillxyz <admin@ourwill.xyz>
2025-07-06 12:45:29 +0300

    Create index.js
    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to OurWill Admin Panel</h1>
+      <p>This is the starting page. Your admin features will go here.</p>
+    </div>
+  );
+}
