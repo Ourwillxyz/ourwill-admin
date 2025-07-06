99157313e13c13c78b1b636532ac723eca814f3a
Ourwillxyz <admin@ourwill.xyz>
2025-07-06 07:20:30 +0300

    Delete pages directory
    Cleanup: Removed old admin pages

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function AdminDashboard() {
-  return (
-    <div>
-      <h1>Welcome to the Admin Dashboard</h1>
-      <p>This is the landing page for admin.ourwill.xyz</p>
-    </div>
-  );
-}
