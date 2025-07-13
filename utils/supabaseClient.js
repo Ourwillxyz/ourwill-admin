08028aa7d66504ce3a169b2d1372d4675fad97bd
Ourwillxyz <admin@ourwill.xyz>
2025-07-13 10:20:41 +0300

    Delete utils/supabaseClient.js

--- utils/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  process.env.https://hhszxyvweibohlaiildw.supabase.co,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhoc3p4eXZ3ZWlib2hsYWlpbGR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3OTY2NjksImV4cCI6MjA2NzM3MjY2OX0.QAdLfaXwhYInGF4lnBEcSNLFeva60iAV5_3Nb5P3gqE
-);
-
-export { supabase };
