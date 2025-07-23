1859b484384ceff5b5356db6bb204025e3675426
Ourwillxyz <admin@ourwill.xyz>
2025-07-23 19:30:48 +0300

    Create user.js

---
+++ src/models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["observer", "candidate", "agent", "returning_officer"],
+    default: "observer"
+  },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("User", userSchema);
